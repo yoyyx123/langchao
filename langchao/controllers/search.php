570844ddbd965a155c064e25fca180efd4074ad5
yoyyx123 <yoyyx123@126.com>
2015-02-02 19:12:22 +0800

    提交

--- langchao/controllers/search.php
+++ langchao/controllers/search.php
@@ -33,15 +33,52 @@ class Search extends MY_Controller {
                 }
             }
             $event_list = $this->Event_model->get_event_search_list($params,$this->per_page);
-            $this->pages_conf($event_list['count']);
-            $this->data['event_list'] = $event_list['info'];
+            if(isset($params['member_id'])&&!empty($params['user_id'])){
+                foreach($event_list as $key=>$val{
+                    $result[$val['user_id']][] = $val;
+                }
+            }elseif(isset($params['member_id'])&&empty($params['user_id'])){
+                foreach($event_list as $key=>$val{
+                    $result[$val['user_id']][] = $val;
+                }                
+            }elseif(!isset($params['member_id'])&&!empty($params['user_id'])){
+                foreach($event_list as $key=>$val{
+                    $result[$val['member_id']][] = $val;
+                }                
+            }
+
+            foreach ($result as $k=>$v){
+                $info[$key]['count'] = count($val);
+                $info[$key]['user_name'] = $val[0]['user_name'];
+                $info[$key]['short_name'] = $val[0]['short_name'];
+                $work_time = 0;
+                $road_time = 0;
+                $complain_num = 0
+                $check_num = 0
+                foreach($v as $kk=>$vv){
+                    if($vv['is_complain']==1){
+                     $complain_num +=1;
+                    }
+                    if($vv['status']==3){
+                     $check_num +=1;
+                    }
+                    $work_time += (strtotime($vv['arrive_time'])-$vv['back_time']);
+                    foreach($vv['bill_order_list'] as $m => $n){
+                        $road_time += (strtotime($n['arrival_time'])-$n['go_time'])
+                    }
+                }
+                $info[$key]['work_time'] = $work_time;
+                $info[$key]['road_time'] = $road_time;
+                $info[$key]['complain_num'] = $complain_num;
+                $info[$key]['check_num'] = $check_num;
+            }            
+            //$this->pages_conf($event_list['count']);
+            $this->data['info_list'] = $info;
         }
-        $where = array();
         $user_list = $this->User_model->get_user_list();
         $this->data['user_list'] = $user_list['info'];
         $this->data['user_data'] = $this->session->userdata;        
         $this->layout->view('search/data_search',$this->data);        
-
     }
 }
 
